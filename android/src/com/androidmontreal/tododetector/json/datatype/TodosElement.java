ee5d318ee5d2b4a85c9bf092fc0aaea70aa41833
Renaud Lepage <root@cybikbase.com>
2012-02-18 19:11:24 -0500

    Housekeeping (watch out for the dot files, eclipse wipe), GitIgnore

---
+++ android/src/com/androidmontreal/tododetector/json/datatype/TodosElement.java
@@ -0,0 +1,18 @@
+package com.androidmontreal.tododetector.json.datatype;
+
+public class TodosElement {
+	private String todoname;
+	private long todoid;
+	public String getTodoname() {
+		return todoname;
+	}
+	public void setTodoname(String todoname) {
+		this.todoname = todoname;
+	}
+	public long getTodoid() {
+		return todoid;
+	}
+	public void setTodoid(long todoid) {
+		this.todoid = todoid;
+	}
+}
