e1bcfd3594cc746ddd6e722440a4b99728671579
cesine <cesine@yahoo.com>
2012-03-10 10:12:08 -0500

    formatting

--- android/src/com/androidmontreal/opencv/AndroidOpenCVforHackathonsActivity.java
+++ android/src/com/androidmontreal/opencv/AndroidOpenCVforHackathonsActivity.java
@@ -25,67 +25,68 @@ import android.widget.TextView;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-
-public class AndroidOpenCVforHackathonsActivity extends Activity implements PictureCallback
-{
+public class AndroidOpenCVforHackathonsActivity extends Activity implements
+		PictureCallback {
 	private static final String TAG = "OpenCVforHackathons";
-	
-	 // OpenCV results view
-    private TextView textView1;
-    private boolean stopUpdatingInBackground = false;
 
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "=onCreate=");
-		
-        /* Create a TextView and set its content.
-         * the text is retrieved by calling a native
-         * function.
-         */
-        setContentView(R.layout.main);
-        textView1 = (TextView) findViewById(R.id.textview1);
-        textView1.setText( stringFromJNI() );
-        
-    }
-    public void getOpenCVResult(View view){
-    	UpdateFromOpenCVTask getOpenCVResults = new UpdateFromOpenCVTask();
-        getOpenCVResults.execute(new String[] { "in execute" });
-    
-    }
-    private class UpdateFromOpenCVTask extends AsyncTask<String, Void, String> {
+	// OpenCV results view
+	private TextView textView1;
+	private boolean stopUpdatingInBackground = false;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "=onCreate=");
+
+		/*
+		 * Create a TextView and set its content. the text is retrieved by
+		 * calling a native function.
+		 */
+		setContentView(R.layout.main);
+		textView1 = (TextView) findViewById(R.id.textview1);
+		textView1.setText(stringFromJNI());
+
+	}
+
+	public void getOpenCVResult(View view) {
+		UpdateFromOpenCVTask getOpenCVResults = new UpdateFromOpenCVTask();
+		getOpenCVResults.execute(new String[] { "in execute" });
+
+	}
+
+	private class UpdateFromOpenCVTask extends AsyncTask<String, Void, String> {
 		@Override
 		protected String doInBackground(String... urls) {
-			Log.d(TAG,"Pausing 1 sec before calling again.");
+			Log.d(TAG, "Pausing 1 sec before calling again.");
 			try {
 				new Thread().sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if(!(stopUpdatingInBackground)){
+			if (!(stopUpdatingInBackground)) {
 				// Loop every 1 sec
 				getOpenCVResult(textView1);
 			}
 			String response = "Hi from the AsyncThread";
-			response = ((AndroidOpenCVforHackathonsApp) getApplication()).getLastMessage(); 
+			response = ((AndroidOpenCVforHackathonsApp) getApplication())
+					.getLastMessage();
 			return response;
 		}
+
 		@Override
 		protected void onPostExecute(String result) {
 			/*
-			 * The result comes from the above doInBackground method, 
-			 * but this runs on the UI thread and so we can set a field in the UI.
+			 * The result comes from the above doInBackground method, but this
+			 * runs on the UI thread and so we can set a field in the UI.
 			 */
 			textView1.setText(result);
 		}
 	}
 
-    @Override
+	@Override
 	protected void onDestroy() {
-    	Log.d(TAG, "=onDestroy=");
+		Log.d(TAG, "=onDestroy=");
 		super.onDestroy();
 	}
 
@@ -99,7 +100,7 @@ public class AndroidOpenCVforHackathonsActivity extends Activity implements Pict
 	protected void onPause() {
 		Log.d(TAG, "====onPause====");
 		stopUpdatingInBackground = true;
-	    super.onPause();
+		super.onPause();
 	}
 
 	@Override
@@ -107,7 +108,7 @@ public class AndroidOpenCVforHackathonsActivity extends Activity implements Pict
 		Log.d(TAG, "==onResume==");
 		super.onResume();
 		stopUpdatingInBackground = false;
-        getOpenCVResult(textView1);
+		getOpenCVResult(textView1);
 	}
 
 	@Override
@@ -125,38 +126,40 @@ public class AndroidOpenCVforHackathonsActivity extends Activity implements Pict
 		Camera camera = previewView.getCamera();
 		camera.takePicture(null, null, this);
 
-    }
-    public void onPictureTaken(byte[] data, Camera camera) {
+	}
+
+	public void onPictureTaken(byte[] data, Camera camera) {
 		/*
 		 * Do some thing
 		 */
 		finish();
 	}
-    /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
-     * with this application.
-     */
-    public native String  stringFromJNI();
 
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-    public native String  unimplementedStringFromJNI();
+	/*
+	 * A native method that is implemented by the 'hello-jni' native library,
+	 * which is packaged with this application.
+	 */
+	public native String stringFromJNI();
 
+	/*
+	 * This is another native method declaration that is *not* implemented by
+	 * 'hello-jni'. This is simply to show that you can declare as many native
+	 * methods in your Java code as you want, their implementation is searched
+	 * in the currently loaded native libraries only the first time you call
+	 * them.
+	 * 
+	 * Trying to call this function will result in a
+	 * java.lang.UnsatisfiedLinkError exception !
+	 */
+	public native String unimplementedStringFromJNI();
 
-    /* this is used to load the 'hello-jni' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.example.HelloJni/lib/libhello-jni.so at
-     * installation time by the package manager.
-     */
-    static {
-        System.loadLibrary("opencv_sample");
-    }
+	/*
+	 * this is used to load the 'hello-jni' library on application startup. The
+	 * library has already been unpacked into
+	 * /data/data/com.example.HelloJni/lib/libhello-jni.so at installation time
+	 * by the package manager.
+	 */
+	static {
+		System.loadLibrary("opencv_sample");
+	}
 }
